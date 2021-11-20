fe42220da2dff9e6bada70850ec84bceced1f33d
hortegab <hortegab@e3t.uis.edu.co>
2021-11-20 15:03:24 +0000

    mejora a la PSD

--- Fase I/Pract3/prac3a.py
+++ Fase I/Pract3/prac3a.py
@@ -37,14 +37,14 @@ import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-import E3TRadio
 import epy_block_0
+import numpy as np
 
 
 
 from gnuradio import qtgui
 
-class pruebapsd(gr.top_block, Qt.QWidget):
+class prac3a(gr.top_block, Qt.QWidget):
 
     def __init__(self):
         gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
@@ -67,7 +67,7 @@ class pruebapsd(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "pruebapsd")
+        self.settings = Qt.QSettings("GNU Radio", "prac3a")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -80,7 +80,7 @@ class pruebapsd(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.h = h = (1,1,1,1)
+        self.h = h = np.array([1,1,1,1])
         self.Sps = Sps = len(h)
         self.Rb = Rb = 32000
         self.samp_rate = samp_rate = Rb*Sps
@@ -94,12 +94,17 @@ class pruebapsd(gr.top_block, Qt.QWidget):
         self.Menu_layout_0 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Menu_widget_0)
         self.Menu_grid_layout_0 = Qt.QGridLayout()
         self.Menu_layout_0.addLayout(self.Menu_grid_layout_0)
-        self.Menu.addTab(self.Menu_widget_0, 'Time')
+        self.Menu.addTab(self.Menu_widget_0, 'Time & Freq')
         self.Menu_widget_1 = Qt.QWidget()
         self.Menu_layout_1 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Menu_widget_1)
         self.Menu_grid_layout_1 = Qt.QGridLayout()
         self.Menu_layout_1.addLayout(self.Menu_grid_layout_1)
-        self.Menu.addTab(self.Menu_widget_1, 'Frec')
+        self.Menu.addTab(self.Menu_widget_1, 'Freq')
+        self.Menu_widget_2 = Qt.QWidget()
+        self.Menu_layout_2 = Qt.QBoxLayout(Qt.QBoxLayout.TopToBottom, self.Menu_widget_2)
+        self.Menu_grid_layout_2 = Qt.QGridLayout()
+        self.Menu_layout_2.addLayout(self.Menu_grid_layout_2)
+        self.Menu.addTab(self.Menu_widget_2, 'Time')
         self.top_grid_layout.addWidget(self.Menu)
         self.qtgui_vector_sink_f_0 = qtgui.vector_sink_f(
             N,
@@ -107,19 +112,19 @@ class pruebapsd(gr.top_block, Qt.QWidget):
             samp_rate/N,
             "f",
             "Sx(f)",
-            "PSD",
+            "PSD (Watts/Hz)",
             1, # Number of inputs
             None # parent
         )
         self.qtgui_vector_sink_f_0.set_update_time(0.10)
-        self.qtgui_vector_sink_f_0.set_y_axis(-140, 10)
-        self.qtgui_vector_sink_f_0.enable_autoscale(True)
+        self.qtgui_vector_sink_f_0.set_y_axis(0, 1/Rb)
+        self.qtgui_vector_sink_f_0.enable_autoscale(False)
         self.qtgui_vector_sink_f_0.enable_grid(False)
         self.qtgui_vector_sink_f_0.set_x_axis_units("Hz")
         self.qtgui_vector_sink_f_0.set_y_axis_units("Watss/Hz")
         self.qtgui_vector_sink_f_0.set_ref_level(0)
 
-        labels = ['', '', '', '', '',
+        labels = ['p4', '', '', '', '',
             '', '', '', '', '']
         widths = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
@@ -138,33 +143,33 @@ class pruebapsd(gr.top_block, Qt.QWidget):
             self.qtgui_vector_sink_f_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_vector_sink_f_0_win = sip.wrapinstance(self.qtgui_vector_sink_f_0.pyqwidget(), Qt.QWidget)
-        self.Menu_grid_layout_1.addWidget(self._qtgui_vector_sink_f_0_win, 2, 1, 1, 1)
-        for r in range(2, 3):
-            self.Menu_grid_layout_1.setRowStretch(r, 1)
-        for c in range(1, 2):
-            self.Menu_grid_layout_1.setColumnStretch(c, 1)
-        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
+        self.Menu_grid_layout_0.addWidget(self._qtgui_vector_sink_f_0_win, 3, 0, 1, 1)
+        for r in range(3, 4):
+            self.Menu_grid_layout_0.setRowStretch(r, 1)
+        for c in range(0, 1):
+            self.Menu_grid_layout_0.setColumnStretch(c, 1)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
             16*Sps, #size
             samp_rate, #samp_rate
-            "Rect signal", #name
+            "", #name
             1, #number of inputs
             None # parent
         )
-        self.qtgui_time_sink_x_0_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0_0.set_y_axis(-2, 2)
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1.50, 1.50)
 
-        self.qtgui_time_sink_x_0_0.set_y_label('Amplitude', "")
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
 
-        self.qtgui_time_sink_x_0_0.enable_tags(True)
-        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0_0.enable_grid(False)
-        self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0_0.enable_stem_plot(False)
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
 
 
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+        labels = ['p4', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
         widths = [3, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
@@ -180,58 +185,6 @@ class pruebapsd(gr.top_block, Qt.QWidget):
 
         for i in range(1):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
-            else:
-                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
-
-        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.pyqwidget(), Qt.QWidget)
-        self.Menu_grid_layout_0.addWidget(self._qtgui_time_sink_x_0_0_win, 2, 1, 1, 1)
-        for r in range(2, 3):
-            self.Menu_grid_layout_0.setRowStretch(r, 1)
-        for c in range(1, 2):
-            self.Menu_grid_layout_0.setColumnStretch(c, 1)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
-            16, #size
-            Rb, #samp_rate
-            "", #name
-            1, #number of inputs
-            None # parent
-        )
-        self.qtgui_time_sink_x_0.set_update_time(0.10)
-        self.qtgui_time_sink_x_0.set_y_axis(-2, 2)
-
-        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
-
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(True)
-
-
-        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
-            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [3, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        colors = ['blue', 'red', 'green', 'black', 'cyan',
-            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
-        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
-            1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [1, 1, 1, 1, 1,
-            1, 1, 1, 1, 1]
-        markers = [0, -1, -1, -1, -1,
-            -1, -1, -1, -1, -1]
-
-
-        for i in range(1):
-            if len(labels[i]) == 0:
                 self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
             else:
                 self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
@@ -242,10 +195,10 @@ class pruebapsd(gr.top_block, Qt.QWidget):
             self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.Menu_grid_layout_0.addWidget(self._qtgui_time_sink_x_0_win, 1, 1, 1, 1)
-        for r in range(1, 2):
+        self.Menu_grid_layout_0.addWidget(self._qtgui_time_sink_x_0_win, 2, 0, 1, 1)
+        for r in range(2, 3):
             self.Menu_grid_layout_0.setRowStretch(r, 1)
-        for c in range(1, 2):
+        for c in range(0, 1):
             self.Menu_grid_layout_0.setColumnStretch(c, 1)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_f(
             N, #size
@@ -289,45 +242,51 @@ class pruebapsd(gr.top_block, Qt.QWidget):
             self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.Menu_grid_layout_1.addWidget(self._qtgui_freq_sink_x_0_win, 1, 1, 1, 1)
-        for r in range(1, 2):
+        self.Menu_grid_layout_1.addWidget(self._qtgui_freq_sink_x_0_win, 3, 0, 1, 1)
+        for r in range(3, 4):
             self.Menu_grid_layout_1.setRowStretch(r, 1)
-        for c in range(1, 2):
+        for c in range(0, 1):
             self.Menu_grid_layout_1.setColumnStretch(c, 1)
         self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_fff(Sps, h)
         self.interp_fir_filter_xxx_0.declare_sample_delay(0)
-        self.fft_vxx_0 = fft.fft_vcc(1024, True, window.blackmanharris(1024), True, 1)
+        self.fft_vxx_0 = fft.fft_vfc(N, True, [1]*N, True, 1)
         self.epy_block_0 = epy_block_0.blk(N=N)
-        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_gr_complex*1, N)
-        self.blocks_int_to_float_0 = blocks.int_to_float(1, 1)
-        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_stream_to_vector_0 = blocks.stream_to_vector(gr.sizeof_float*1, N)
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_multiply_const_vxx_1 = blocks.multiply_const_vff([1/(N*samp_rate)]*N)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(2.)
         self.blocks_complex_to_mag_squared_0 = blocks.complex_to_mag_squared(N)
-        self.analog_random_source_x_0 = blocks.vector_source_i(list(map(int, numpy.random.randint(0, 2, 1000000))), True)
-        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0)
-        self.E3TRadio_unipolar_to_bipolar_ff_0 = E3TRadio.unipolar_to_bipolar_ff(1)
+        self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
+        self.blocks_add_xx_0 = blocks.add_vff(1)
+        self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 2, 1000000))), True)
+        self.analog_noise_source_x_0 = analog.noise_source_f(analog.GR_GAUSSIAN, 1, 76)
+        self.analog_const_source_x_0_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, -1./2.)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.E3TRadio_unipolar_to_bipolar_ff_0, 0), (self.interp_fir_filter_xxx_0, 0))
-        self.connect((self.E3TRadio_unipolar_to_bipolar_ff_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.analog_const_source_x_0, 0), (self.blocks_float_to_complex_0, 1))
-        self.connect((self.analog_random_source_x_0, 0), (self.blocks_int_to_float_0, 0))
+        self.connect((self.analog_const_source_x_0_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_null_sink_0, 0))
+        self.connect((self.analog_random_source_x_0, 0), (self.blocks_char_to_float_0, 0))
+        self.connect((self.blocks_add_xx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_char_to_float_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.blocks_complex_to_mag_squared_0, 0), (self.epy_block_0, 0))
-        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_stream_to_vector_0, 0))
-        self.connect((self.blocks_int_to_float_0, 0), (self.E3TRadio_unipolar_to_bipolar_ff_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.interp_fir_filter_xxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_1, 0), (self.qtgui_vector_sink_f_0, 0))
         self.connect((self.blocks_stream_to_vector_0, 0), (self.fft_vxx_0, 0))
-        self.connect((self.epy_block_0, 0), (self.qtgui_vector_sink_f_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_to_vector_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.epy_block_0, 0), (self.blocks_multiply_const_vxx_1, 0))
         self.connect((self.fft_vxx_0, 0), (self.blocks_complex_to_mag_squared_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_float_to_complex_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.qtgui_freq_sink_x_0, 0))
-        self.connect((self.interp_fir_filter_xxx_0, 0), (self.qtgui_time_sink_x_0_0, 0))
+        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_throttle_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "pruebapsd")
+        self.settings = Qt.QSettings("GNU Radio", "prac3a")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -355,15 +314,17 @@ class pruebapsd(gr.top_block, Qt.QWidget):
     def set_Rb(self, Rb):
         self.Rb = Rb
         self.set_samp_rate(self.Rb*self.Sps)
-        self.qtgui_time_sink_x_0.set_samp_rate(self.Rb)
+        self.qtgui_vector_sink_f_0.set_y_axis(0, 1/self.Rb)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.blocks_multiply_const_vxx_1.set_k([1/(self.N*self.samp_rate)]*self.N)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
-        self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.qtgui_vector_sink_f_0.set_x_axis(-self.samp_rate/2, self.samp_rate/self.N)
 
     def get_N(self):
@@ -371,13 +332,14 @@ class pruebapsd(gr.top_block, Qt.QWidget):
 
     def set_N(self, N):
         self.N = N
+        self.blocks_multiply_const_vxx_1.set_k([1/(self.N*self.samp_rate)]*self.N)
         self.epy_block_0.N = self.N
         self.qtgui_vector_sink_f_0.set_x_axis(-self.samp_rate/2, self.samp_rate/self.N)
 
 
 
 
-def main(top_block_cls=pruebapsd, options=None):
+def main(top_block_cls=prac3a, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
